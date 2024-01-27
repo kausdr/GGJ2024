cef2e757ffb0e1f5309431881c5fadd531d6383e
Kauane <86927114+kausdr@users.noreply.github.com>
2024-01-26 23:46:53 -0300

    GGJ24 first commit

---
+++ GGJ2024/GGJ2024App.swift
@@ -0,0 +1,17 @@
+//
+//  GGJ2024App.swift
+//  GGJ2024
+//
+//  Created by Kauane Santana on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GGJ2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
