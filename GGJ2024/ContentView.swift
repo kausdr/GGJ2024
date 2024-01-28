c592b229946fc6b1a861dbcd3b848ab10ab9f55d
Andrea Oquendo <oquendo.3455@gmail.com>
2024-01-28 09:52:34 -0300

    feat: home view com inicio

--- GGJ2024/ContentView.swift
+++ GGJ2024/ContentView.swift
@@ -9,20 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var botaoJogar = false
     
     var body: some View {
         NavigationStack{
             
+            NavigationLink(destination: GameplayView(), isActive: $botaoJogar) {
+                EmptyView()
+            }
+            
             ZStack{
                 
                 BackgroundView()
+                
+                HStack(alignment: .bottom){
+                    VStack{
+                        Image("logo_pigeon")
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                .padding(.top, 60)
+                .padding(.horizontal, 55)
+                    
+                
+                Image("pombo_inicio")
+                    .resizable()
+                    .frame(width:274, height: 301)
+                    .offset(x: -20,y: 15)
                 
                 HStack{
                     
                     Spacer()
                     
                     VStack(spacing: 20){
-                        NavigationLink(destination: GameplayView()){
+                        
+                        Button {
+                            SoundManager.instance.playSound(sound: .botao1Menu)
+                            botaoJogar = true
+                        } label: {
                             Image("botao_jogar")
                         }
                         
